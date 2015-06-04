855b4848435546c81af83d70bcf2317943b30d67
vrga <fv.pavelic@egzodus.com>
2015-06-04 21:20:20 +0200

    updated docs for abstract classes.

--- devicecontainers/inputdevice.py
+++ devicecontainers/inputdevice.py
@@ -2,6 +2,9 @@ from abc import ABCMeta, abstractmethod
 
 
 class InputDevice(metaclass=ABCMeta):
+    """
+    Abstract class for input devices.
+    """
     @abstractmethod
     def get_temp(self):
         pass
--- devicecontainers/outputdevice.py
+++ devicecontainers/outputdevice.py
@@ -2,10 +2,17 @@ from abc import ABCMeta, abstractmethod
 
 
 class OutputDevice(metaclass=ABCMeta):
+    """
+    Abstract class for output devices.
+    """
     @abstractmethod
-    def set_speed(self):
+    def set_speed(self, speed):
         pass
 
     @abstractmethod
     def enable(self):
+        pass
+
+    @abstractmethod
+    def disable(self):
         pass
