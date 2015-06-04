f67e27571e86e11c849f7f55374f688e4104ffa9
vrga <fv.pavelic@egzodus.com>
2015-06-04 21:10:06 +0200

    initial controllers commit.

---
+++ controllers/__init__.py
@@ -0,0 +1,1 @@
+from .temperaturecontroller import *
---
+++ controllers/temperaturecontroller.py
@@ -0,0 +1,34 @@
+import logging
+
+
+class TemperatureController(object):
+    """
+        raw temperature controller
+    """
+
+    def __init__(self, input_device, output_device, speeds):
+        """
+        :param input_device: Input device from which we take the temperature.
+        :type input_device: InputDevice
+        :param output_device: Output device to which we set the speed
+        :type output_device: OutputDevice
+        :param speeds: tuple of speeds to which we set it.
+        :type speeds: tuple
+        """
+        self.input = input_device
+        self.output = output_device
+        self.speeds = speeds
+        self.temp = 0
+        self.speed = 0
+
+    def run(self):
+        """
+            method which runs the thing.
+            If reported temperature is 0, take previous temp
+        """
+        temp = self.input.get_temp()
+        if temp != 0:
+            self.temp = temp
+        self.speed = self.speeds[self.temp]
+        logging.debug('temperature %s°C, speed: %s%', self.temp, self.speed)
+        self.output.set_speed(self.speed)
